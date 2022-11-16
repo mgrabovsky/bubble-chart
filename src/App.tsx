4aa49e03fc20f6f95e516378f36830e99625327e
Matěj Grabovský <matej@mgrabovsky.net>
2022-11-16 19:18:13 +0100

    Kick off

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>An Etude on Human Progress</h1>
+      <p>Hello, world! There’s going to be a bubble chart here soon.</p>
+    </div>
+  );
+}
