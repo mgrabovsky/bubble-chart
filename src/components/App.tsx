3e294befced8a288aac0b43971cdab5c0659d303
Matěj Kolouch Grabovský <matej@mgrabovsky.eu>
2025-06-25 11:55:43 +0200

    Move dataset filtering into BubbleChart

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 import datasetUrl from '../data/dataset.json?url';
@@ -10,26 +10,6 @@ import { YearSelector } from './YearSelector';
 const CHART_SIZE = { height: 400, width: 600 };
 const CHART_MARGINS = { top: 20, right: 40, bottom: 45, left: 50 };
 const DEFAULT_YEAR = 2000;
-
-interface FilterCriteria {
-  continent?: Continent;
-  year: number;
-}
-
-/**
- * Filter the dataset for entries matching the given criteria.
- * Returns a new dataset.
- */
-function filterDataset(dataset: Dataset, criteria: FilterCriteria) {
-  const { continent, year } = criteria;
-  return (
-    dataset
-      .filter((d) => d.year === year && (continent ? d.continent === continent : true))
-      // Sort by descending population so that the biggest countries are rendered
-      // first and that the smaller ones are hoverable.
-      .sort((d1, d2) => d2.population - d1.population)
-  );
-}
 
 /**
  * Root component of the bubble chart application.
@@ -47,11 +27,7 @@ export function App() {
     fetchDataset(datasetUrl).then(setDataset);
   }, []);
 
-  // Display only data matching the selected criteria.
-  const filteredData = useMemo<Dataset | undefined>(() => {
-    if (!dataset || !dataset.length) return;
-    return filterDataset(dataset, { continent, year });
-  }, [continent, dataset, year]);
+  const filterCriteria = { continent, year };
 
   return (
     <div className="App">
@@ -76,7 +52,7 @@ export function App() {
         </p>
       </div>
       {isLoading && <p>Loading data…</p>}
-      {filteredData && (
+      {dataset && (
         <>
           <YearSelector
             max={yearRange[1]}
@@ -85,7 +61,8 @@ export function App() {
             value={year}
           />
           <BubbleChart
-            data={filteredData}
+            criteria={filterCriteria}
+            data={dataset}
             height={CHART_SIZE.height}
             margin={CHART_MARGINS}
             width={CHART_SIZE.width}
--- src/dataset.ts
+++ src/dataset.ts
@@ -7,7 +7,7 @@ export const CONTINENTS = [
   'South America',
 ] as const;
 
-export type Continent = typeof CONTINENTS[number];
+export type Continent = (typeof CONTINENTS)[number];
 
 export interface DatasetRow {
   code: string;
@@ -21,6 +21,26 @@ export interface DatasetRow {
 
 export type Dataset = DatasetRow[];
 
+export interface FilterCriteria {
+  continent?: Continent;
+  year: number;
+}
+
 export async function fetchDataset(url: string): Promise<Dataset> {
   return fetch(url).then((response) => response.json());
+}
+
+/**
+ * Filter the dataset for entries matching the given criteria.
+ * Returns a new dataset.
+ */
+export function filterDataset(dataset: Dataset, criteria: FilterCriteria) {
+  const { continent, year } = criteria;
+  return (
+    dataset
+      .filter((d) => d.year === year && (continent ? d.continent === continent : true))
+      // Sort by descending population so that the biggest countries are rendered
+      // first and that the smaller ones are hoverable.
+      .sort((d1, d2) => d2.population - d1.population)
+  );
 }
